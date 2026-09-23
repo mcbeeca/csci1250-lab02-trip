3d0b30fca6e06b258a368ea22c992f5ded544064
Chase McBee <chasamcbee@tutamail.com>
2026-09-23 16:08:13 -0400

    Add pizza party calculator

--- TripCalculator/Program.cs
+++ TripCalculator/Program.cs
@@ -23,3 +23,27 @@ Console.WriteLine("Miles per gallon: " + milesPerGallon.ToString("F2"));
 Console.WriteLine("Price per gallon: " + pricePerGallon.ToString("C2"));
 Console.WriteLine("\nGallons needed: " + gallonsNeeded.ToString("F2"));
 Console.WriteLine("Fuel cost: " + fuelCost.ToString("C"));
+
+//Description: Calculates the total slices for each person at a party, along with the total cost of all of the pizzas.
+
+Console.Write("\nHow many people are coming to the party?: ");
+int people = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("How many pizzas?: ");
+int pizza = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Price per pizza?: ");
+double price = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("\n=====Part 2: Pizza Party=====");
+const int slices = 8;
+int totalSlices = pizza * slices;
+Console.WriteLine("Total slices: " + totalSlices);
+
+double slicesPerPerson = totalSlices / (double)people;
+Console.WriteLine("Slices per person: " + slicesPerPerson.ToString("F1"));
+
+double pizzaCost = pizza * price;
+Console.WriteLine("Pizza cost: " + pizzaCost.ToString("C"));
+
+//Description: Calculates gross pay, tax witheld, and take home pay for a paycheck.
